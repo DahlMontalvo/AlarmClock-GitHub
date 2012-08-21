5f697054e63db5977d9bc5951b06d83a6b7bbed3
Jonas Dahl <jonas@jdahl.se>
2012-08-21 22:35:36 +0200

    Centraliserat settingsarna lite - strukturerat om koden

--- AlarmClock-GitHub/alarm1/SettingsViewController.h
+++ AlarmClock-GitHub/alarm1/SettingsViewController.h
@@ -2,31 +2,12 @@
 //  SettingsViewController.h
 //  alarm1
 //
-//  Created by Philip Montalvo on 2012-08-06.
+//  Created by Jonas Dahl on 8/21/12.
 //
 //
 
 #import <UIKit/UIKit.h>
-@class SettingsViewController;
-@protocol SettingsViewControllerDelegate <NSObject>
-- (void)settingsViewControllerDidCancel:(SettingsViewController *)controller;
-- (void)settingsViewControllerDidSave:(SettingsViewController *)controller;
 
-@end
-
-@interface SettingsViewController : UIViewController {
-    
-}
-
-
-@property (nonatomic, weak) id <SettingsViewControllerDelegate> delegate;
-
-
-- (IBAction)cancel:(id)sender;
-- (IBAction)save:(id)sender;
-
-
-
-
+@interface SettingsViewController : UITableViewController
 
 @end
--- AlarmClock-GitHub/alarm1/SettingsViewController.m
+++ AlarmClock-GitHub/alarm1/SettingsViewController.m
@@ -2,27 +2,21 @@
 //  SettingsViewController.m
 //  alarm1
 //
-//  Created by Philip Montalvo on 2012-08-06.
+//  Created by Jonas Dahl on 8/21/12.
 //
 //
 
 #import "SettingsViewController.h"
 
+@interface SettingsViewController ()
+
+@end
+
 @implementation SettingsViewController
-@synthesize delegate;
 
--(IBAction)cancel:(id)sender {
-    [self.delegate settingsViewControllerDidCancel:self];
-}
-
--(IBAction)save:(id)sender {
- // Do stuff here
-     [self.delegate settingsViewControllerDidSave:self];
-}
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithStyle:(UITableViewStyle)style
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithStyle:style];
     if (self) {
         // Custom initialization
     }
@@ -32,18 +26,102 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
 }
 
 @end
